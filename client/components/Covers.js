18ee4788321aad5adaad0134c0fe71868025e823
retr0mouse <daniil.sharin667@gmail.com>
2022-01-28 13:12:39 +0200

    styled components implementation

--- client/components/Covers.js
+++ client/components/Covers.js
@@ -1,19 +1,31 @@
+import styled from "styled-components";
+
+const Cover = styled.div`
+    background-color: ${props => props.selected ? "rgb(129, 179, 64)" : "none"};
+    padding: ${props => props.selected ? "5px" : 0};
+`;
+
+const CoverContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
 export function Covers({items, selectedItem, onSelected}) {
     return (
         <>
-            <div className="cover-container">
+            <CoverContainer>
                 {items?.map((item, index) => {
                     return(
-                        <div 
+                        <Cover
                             key={index} 
-                            className={selectedItem === item ? "selected-cover" : ""}
+                            selected={selectedItem === item}
                         >
                             <img width="128px" src={item}></img><br />
                             <button onClick={() => onSelected(item)}>✅</button>
-                        </div>
+                        </Cover>
                     )
                 })}
-            </div>
+            </CoverContainer>
         </>
     );
 }
