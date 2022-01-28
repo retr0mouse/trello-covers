94d53de15ee64894f3fffb0708f8868dd9968a8b
retr0mouse <daniil.sharin667@gmail.com>
2022-01-28 13:40:21 +0200

    remove comments

--- client/App.js
+++ client/App.js
@@ -187,8 +187,3 @@ export default function App() {
         setMovies(filteredMovies);
     }
 };
-
-
-//d0256790589a55b455aab52402dfc7bd - TMDB key
-//eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMDI1Njc5MDU4OWE1NWI0NTVhYWI1MjQwMmRmYzdiZCIsInN1YiI6IjYxZjI1OTU2NTU5ZDIyMDEwNWNhMDZiNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._asLbJ7VJLrX69lbtJ0xfX7G8zLhyf1TzEy3Jhi_vW4 - TMDB token
-//https://api.themoviedb.org/3/search/movie/?api_key=d0256790589a55b455aab52402dfc7bd&query=JoJo - example use
